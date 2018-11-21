16d7f361e66140d8675c75be696bb38ef48fd03d
unknown <alejna94@gmail.com>
2018-11-21 11:31:00 +0100

    Initial commit

---
+++ server/utils/message.test.js
@@ -0,0 +1,34 @@
+var expect=require('expect');
+var {generateMessage,generateLocationMessage}=require('./message');
+
+describe('generateMessage', ()=>{
+
+it('should generate correct message object',()=>{
+  var from='Jen';
+  var text='what are you doing?';
+  var message=generateMessage(from, text);
+
+  expect(message.createdAt).toBeA('number');
+  expect(message).toInclude({from,text});
+
+});
+
+
+});
+
+describe('generateLocationMessage',()=>{
+
+it('should generate correct location object',()=>{
+
+  var from='Jenna';
+  var latitude=2;
+  var longitude=2;
+  var url='https://www.google.com/maps?q=2,2';
+  var message=generateLocationMessage(from,latitude,longitude);
+
+  expect(message.createdAt).toBeA('number');
+  expect(message).toInclude({from,url});
+
+});
+
+});
